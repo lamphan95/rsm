6b8e3a6e2e77eed12c41c1246868314ab4208894
hhhhuy112 <minhhuyho2011@gmail.com>
2018-02-21 15:49:15 +0700

    fix bug apply

--- app/helpers/applies_helper.rb
+++ app/helpers/applies_helper.rb
@@ -1,6 +1,6 @@
 module AppliesHelper
   def get_data appointments, apply
-    content_tag "div", id: "apply-appointments", data: {events: appointments, name: get_name_to(apply)}
+    content_tag "div", "", id: "apply-appointments", data: {events: appointments, name: get_name_to(apply)}
   end
 
   def get_name_to apply
