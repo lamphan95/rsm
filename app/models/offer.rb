08a1850823103136a04f6669dc00d8b99d30a339
lamphan <dinhlam29995@gmail.com>
2018-02-27 11:34:18 +0700

    fix validate offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,8 +5,15 @@ class Offer < ApplicationRecord
   validates :salary, presence: true
   validates :start_time, presence: true
   validates :address, presence: true
+  validate :start_time_cannot_be_in_the_past, if: :start_time?
 
   delegate :name, to: :user, prefix: true, allow_nil: true
 
   scope :get_newest, ->{order created_at: :desc}
+
+  def start_time_cannot_be_in_the_past
+    if start_time < DateTime.now
+      errors.add :start_time, I18n.t(".start_time_cannot_be_in_the_past")
+    end
+  end
 end
