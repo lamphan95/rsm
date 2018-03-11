6abb9c9f85c402d54807187ea32b2d8fa865c753
Vo Van Tri <vo.van.tri@framgia.com>
2018-03-11 23:55:35 +0700

    add columns update date

--- spec/controllers/employers/companies_controller_spec.rb
+++ spec/controllers/employers/companies_controller_spec.rb
@@ -4,6 +4,6 @@ RSpec.describe Employers::CompaniesController, type: :controller do
   describe "GET index" do
     it "has a 200 status code" do
       get :index
-      end
+    end
   end
 end
--- spec/models/apply_spec.rb
+++ spec/models/apply_spec.rb
@@ -5,6 +5,12 @@ RSpec.describe Apply, type: :model do
     it {is_expected.to belong_to :user}
     it {is_expected.to belong_to :job}
     it {is_expected.to have_one :company}
+    it {is_expected.to have_many :inforappointments}
+    it {is_expected.to have_many :apply_statuses}
+    it {is_expected.to have_many :appointments}
+    it {is_expected.to have_many :status_steps}
+    it {is_expected.to have_many :steps}
+    it {is_expected.to have_many :offers}
   end
 
    context "columns" do
@@ -13,5 +19,56 @@ RSpec.describe Apply, type: :model do
     it {is_expected.to have_db_column(:job_id).of_type(:integer)}
     it {is_expected.to have_db_column(:cv).of_type(:text)}
     it {is_expected.to have_db_column(:information).of_type(:text)}
+  end
+
+  context "validates" do
+    it {is_expected.to validate_presence_of(:cv)}
+    it {is_expected.to validate_presence_of(:information)}
+  end
+
+  context "nested attributes for apply_statuses" do
+    it "exits accept_nested_attributes_for apply_statuses " do
+      expect accept_nested_attributes_for(:apply_statuses)
+      expect accept_nested_attributes_for(:apply_statuses).allow_destroy(true)
+      expect accept_nested_attributes_for(:apply_statuses).update_only(true)
+    end
+  end
+
+  context "nested attributes for answers" do
+    it "exits accept_nested_attributes_for answers " do
+      expect accept_nested_attributes_for(:answers)
+      expect accept_nested_attributes_for(:answers).allow_destroy(true)
+    end
+  end
+
+  context "delegate to job" do
+    it "exits delegate_method name to job " do
+      expect delegate_method(:name).to(:job)
+      expect delegate_method(:name).to(:job).with_prefix(true)
+    end
+  end
+
+  context "delegate to step" do
+    it "exits delegate_method name to step " do
+      expect delegate_method(:name).to(:step)
+      expect delegate_method(:name).to(:step).with_prefix(true)
+    end
+  end
+
+  context "delegate to user" do
+    it "exits delegate_method name to user " do
+      expect delegate_method(:name).to(:user)
+      expect delegate_method(:name).to(:user).with_prefix(true)
+    end
+
+    it "exits delegate_method email to user " do
+      expect delegate_method(:email).to(:user)
+      expect delegate_method(:email).to(:user).with_prefix(true)
+    end
+
+    it "exits delegate_method phone to user " do
+      expect delegate_method(:phone).to(:user)
+      expect delegate_method(:phone).to(:user).with_prefix(true)
+    end
   end
 end
